fb9784ac7a84143bbb5897b5bfefb70feea7f7cb
Mikele91 <michele.amoruso91.8@gmail.com>
2021-10-05 17:48:06 +0200

    chiamata axios

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="container">
     <Header @search="searchFilm"/>
 
-    <Films :info="searchText"/>
+    <Films :info="films"/>
     </div>
   </div>
 </template>
@@ -11,6 +11,7 @@
 <script>
 import Films from './components/Films.vue'
 import Header from './components/Header.vue'
+import axios from 'axios';
 
 
 export default {
@@ -22,6 +23,7 @@ export default {
   data(){
     return{
       searchText :"",
+      films:[],
     }
   },
   methods:{
@@ -32,6 +34,26 @@ export default {
   props:{
 
   },
+   created(){
+        // axios.get('https://api.themoviedb.org/3/search/movie',
+        //         {
+        //             params:{
+        //                 api_key: '7e390297db8f760afc45d7092f6f5846',
+        //                 query: searchText,
+        //                 language: 'it-IT',
+        //             }
+        //         }
+        // ).then( (res)=> {
+        //     this.films = res.data.results;
+        // });
+
+
+        axios
+        .get("https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+fut")
+        .then( (res)=> {
+            this.films = res.data.results;
+        })
+    }
 
 }
 </script>
--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <div class="d-flex" v-for= "(film, index) in films" :key="index" >
+  <div class="d-flex" v-for= "(film, index) in info" :key="index" >
     <CardFilm :info= "film"/>
   </div>
 </div>
@@ -9,7 +9,7 @@
 <script>
 import CardFilm from './CardFilm.vue'
 
-import axios from 'axios';
+// import axios from 'axios';
 
 export default {
     name:"Films",
@@ -18,32 +18,51 @@ export default {
     },
     data(){
         return{
-            films:[],
+            // films:[],
         }
     },
     props:{
-        info:String
+        info:Array
         },
-    updated(){
-        axios.get('https://api.themoviedb.org/3/search/movie',
-                {
-                    params:{
-                        api_key: '7e390297db8f760afc45d7092f6f5846',
-                        query: this.info,
-                        language: 'it-IT',
-                    }
-                }
-        ).then( (res)=> {
-            this.films = res.data.results;
-        });
+    //  created(){
+    //     axios.get('https://api.themoviedb.org/3/search/movie',
+    //             {
+    //                 params:{
+    //                     api_key: '7e390297db8f760afc45d7092f6f5846',
+    //                     query: this.info,
+    //                     language: 'it-IT',
+    //                 }
+    //             }
+    //     ).then( (res)=> {
+    //         this.films = res.data.results;
+    //     });
+
+    //     // axios
+    //     // .get("https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+fut")
+    //     // .then( (res)=> {
+    //     //     this.films = res.data.results;
+    //     // })
+    // },
+    //  watch(){
+    //     axios.get('https://api.themoviedb.org/3/search/movie',
+    //             {
+    //                 params:{
+    //                     api_key: '7e390297db8f760afc45d7092f6f5846',
+    //                     query: this.info,
+    //                     language: 'it-IT',
+    //                 }
+    //             }
+    //     ).then( (res)=> {
+    //         this.films = res.data.results;
+    //     });
 
 
-        // axios
-        // .get("https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+fut")
-        // .then( (res)=> {
-        //     this.films = res.data.results;
-        // })
-    }
+    //     // axios
+    //     // .get("https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+fut")
+    //     // .then( (res)=> {
+    //     //     this.films = res.data.results;
+    //     // })
+    // },
 }
 </script>
 
