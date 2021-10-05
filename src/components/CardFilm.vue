85550f8476a0c6af49dc233e2c91cf05f123696c
Mikele91 <michele.amoruso91.8@gmail.com>
2021-10-05 15:35:53 +0200

    aggiunto film in maniera statica

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="container"></div>
+  <div class="film">
+       <!-- <img :src=`https://image.tmdb.org/t/p/w300${backdrop_path}` :alt="info.original_title"> -->
+        <h3>{{info.original_title}}</h3>
+        <div>{{info.title}}</div>
+        <div><strong>{{info.original_language}}</strong></div>
+  </div>
 </template>
 
 <script>
 export default {
     name:"CardFilms",
+    props:{
+      info: Object
+    }
 
 }
 </script>
--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="container">
-      <CardFilms/>
+<div>
+  <div v-for= "(film, index) in films" :key="index" >
+      <CardFilms :info= "film"/>
   </div>
+</div>
 </template>
 
 <script>
@@ -17,14 +19,31 @@ export default {
     data(){
         return{
             films:[],
+            queryVar:""
         }
     },
     props:["info"],
+    methods:{
+        
+        
+    },
     created(){
+        // axios.get('https://api.themoviedb.org/3/search/movie',
+        //         {
+        //             params:{
+        //                 apy_key: '7e390297db8f760afc45d7092f6f5846',
+        //                 query: this.queryVar,
+        //                 language: 'it-IT'
+
+        //             }
+        //         }
+        // )
+
+
         axios
-        .get("https://api.sampleapis.com/rickandmorty/characters")
+        .get("https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+fut")
         .then( (res)=> {
-            this.films = res.data;
+            this.films = res.data.results;
         })
     }
 }
