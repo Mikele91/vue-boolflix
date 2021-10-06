5f22fb62e8208a3e782d2e4a77140537eb564607
Mikele91 <michele.amoruso91.8@gmail.com>
2021-10-06 17:27:28 +0200

    stampare stella

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="film">
-        <img :src="(info.poster_path!= null)?`https://image.tmdb.org/t/p/w300${info.poster_path}`:'https://via.placeholder.com/300x300?text=Image+NO+Found'" :alt="info.original_title">
+        <img :src="(info.poster_path!= null)?`https://image.tmdb.org/t/p/w300${info.poster_path}`:'https://via.placeholder.com/300x300/000000/FFFFFF?text=Image+NO+Found'" :alt="info.original_title">
         <h3>{{info.original_title||info.original_name}}</h3>
         <div>{{info.title||info.name}}</div>
         <!-- <div>{{info.original_language}}</div> -->
-        <div>{{info.vote_count}}</div>
+        <div>{{printStar(vote(info.vote_average))}}</div>
 
         <div class="lang"><lang-flag :iso='info.original_language' :squared="false"/></div>      
   </div>
@@ -16,20 +16,41 @@ import LangFlag from 'vue-lang-code-flags'
 export default {
     name:"CardFilms",
     components: {
-        LangFlag
+        LangFlag,
     },
     props:{
       info: Object,
       // serieTv:Object
-    }
+    },
+    methods:{
+      vote: function(num){
+        let changeStars =  Math.ceil(num/2);
+        return changeStars
+    },
 
+    printStar: function(elm) {
+        let star=[];
+        for(let i = 0; i < 5 ; i++){
+          if (elm < 5){
+            star +=star.push('<i class="fas fa-star"></i> piena');
+          } else{
+            
+            star+= star.push('<i class="far fa-star"></i> vuota');
+
+          }
+          return star
+        }
+
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-
+@import '../assets/style/common.scss';
 </style>
 
 
-468x60?text=Visit+Blogging.com+Now
+
+
 
